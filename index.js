8d8ea3d690480bd2a8bca4b852c34e5e4a70853f
Joseph <josephgrosenfeld@gmail.com>
2022-01-18 11:48:41 -0500

    added response for http get on root

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("why hello there");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
