c494aa049e41a9723f45ee9e46d63b5719d9ac00
Joseph <josephgrosenfeld@gmail.com>
2022-01-19 11:13:56 -0500

    added scripts and spec file

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("why hello there");
+  res.send("<h1>Hello There</h1>");
+});
+
+app.get("/products", (req, res) => {
+  res.send([
+    {
+      product_id: 1,
+      price: "$10",
+    },
+    {
+      product_id: 2,
+      price: "$20",
+    },
+  ]);
 });
 
 const PORT = process.env.PORT || 5000;
