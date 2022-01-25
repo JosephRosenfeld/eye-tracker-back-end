31575bdf54f07e6f2dbcb3bc015584836dc3d8ec
Joseph <josephgrosenfeld@gmail.com>
2022-01-25 14:39:41 -0500

    updated to remove db interaction for AWS testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import compression from "compression";
 import session from "express-session";
-import pool from "./config/db.js";
+//import pool from "./config/db.js";
 import connectPgSimple from "connect-pg-simple";
 import bcrypt from "bcrypt";
 
@@ -17,24 +17,24 @@ const app = express();
 
 /*--- Middlewares ---*/
 //Need cors options since we're dealing with cookies
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors(/*{ credentials: true, origin: "http://localhost:3000" }*/));
 app.use(express.json());
 app.use(compression());
-app.use(
+/*app.use(
   session({
     store: new pgSession({
       pool: pool,
       createTableIfMissing: true,
     }),
     secret: "key that will sign the cookie, could be any random string",
-    resave: false /*stops the session from being saved back to the store when no changes were made during the request*/,
-    saveUninitialized: false /*doesn't save a cookie if we don't set anything on session*/,
-    cookie: {
+    resave: false /*stops the session from being saved back to the store when no changes were made during the request*/ /*,
+    /*saveUninitialized: false /*doesn't save a cookie if we don't set anything on session*/ /*,
+    /*cookie: {
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: false,
-    } /*1000 miliseconds = second; 60 secs = 1min; 60 mins = 1hr; 24 hrs = 1d;*/,
+    } /*1000 miliseconds = second; 60 secs = 1min; 60 mins = 1hr; 24 hrs = 1d;*/ /*,
   })
-);
+);*/
 
 const dayOfLogs = [
   { type: "systane", date: "05-09-2000 05:20:22Z" },
@@ -86,7 +86,7 @@ app.get("/api/test", (req, res) => {
   res.status(200).send(JSON.stringify({ logs: allLogs }));
 });
 
-/*--- API Auth Section ---*/
+/*--- API Auth Section ---*/ /*
 app.post("/api/auth/loginadmin", async (req, res) => {
   try {
     //get admin pwd from db
@@ -104,7 +104,7 @@ app.post("/api/auth/loginadmin", async (req, res) => {
       lib to store a cookie on the user. If the user already had a cookie from being
       logged in as a guest, then no worries and it should just overwrite the server 
       side specific session variable we're setting (in this case 'isAdmin and isGuest') 
-      and not change the actual client side cookie*/
+      and not change the actual client side cookie*/ /*
       req.session.user = {
         isAdmin: true,
         isGuest: false,
@@ -133,7 +133,7 @@ app.get("/api/auth/loginguest", (req, res) => {
   res.sendStatus(200);
 
   /*Some logic to copy all data from db on the only row item and then put it all
-  in the session storage*/
+  in the session storage*/ /*
 });
 
 app.get("/api/auth/logincheck", (req, res) => {
@@ -161,4 +161,6 @@ app.use("/api/settings", settingsRoutes);
 */
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT} at time ${new Date()}`)
+);
