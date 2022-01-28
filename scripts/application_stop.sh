402a7a3446cc9cace77d25afc03359a901f0dfea
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 12:14:43 -0500

    removed kill ngrok

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,8 +3,4 @@
 
 sudo -i
 echo "Stopping any existing servers (echo without stdout specified)" 
-  {
-    echo "Stopping any existing node servers!"  
-    sudo pkill -9 node 
-    sudo pkill -9 ngrok  
-  } > app_stop.out.log 2> app_stop.err.log &
+pkill -9 node 
