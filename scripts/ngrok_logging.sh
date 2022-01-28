527a081875a6592911c70fb2b50747c378c27fb2
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 13:01:32 -0500

    edits to push

--- scripts/ngrok_logging.sh
+++ scripts/ngrok_logging.sh
@@ -2,5 +2,4 @@
 
 #get log of ngrok urls
 sudo -i
-curl -s http://localhost:4040/api/tunnels | jq ".tunnels[1].public_url" > tunnels.log
-curl -s http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url" >> tunnels.log
+curl -s http://localhost:5000/
