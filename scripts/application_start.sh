a5aaa3fee6fa665653e2922248b08d9acba25b77
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 10:30:15 -0500

    added ngrok_loggin script, and tried fix for app stop script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -17,9 +17,6 @@ npm install
 #start our node app in the background
 node index.js > app.out.log 2> app.err.log < /dev/null & 
 
-#start ngrok in background
+#start ngrok in background (Its important to redirect outputs or else AWS will give you
+#a silent hang)
 ngrok http 5000 > ngrok.out.log 2> app.err.log < /dev/null &
-
-#get log of ngrok urls
-curl -s http://localhost:4040/api/tunnels | jq ".tunnels[1].public_url" > tunnels.log
-curl -s http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url" >> tunnels.log
