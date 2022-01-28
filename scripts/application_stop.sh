79d9a6a2ac20ffc56c6d886ad9f4c82bcfcdd18b
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 11:26:55 -0500

    changed app_stop for two echos

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers !"
+echo "Stopping any existing servers (echo without stdout specified)"  
+
 {
+echo "Stopping any existing node servers!"  
 pkill -9 node 
 pkill -9 ngrok
 } > app_stop_.out.log 2> app_stop.err.log
