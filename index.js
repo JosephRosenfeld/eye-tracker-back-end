36ca682339172295d6b2b3136462556b655749c9
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 12:44:43 -0500

    testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import pool from "./config/db.js";
 import connectPgSimple from "connect-pg-simple";
 import bcrypt from "bcrypt";
 
-//comments for pushing changes aasd
+//comments for pushing changes
 
 //import logRoutes from "./routes/logRoutes";
 //import settingsRoutes from "./routes/settingsRoutes";
