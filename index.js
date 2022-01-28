147a88632431714aba1ca479f7ee863e54ba132e
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 13:16:56 -0500

    edits to push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import pool from "./config/db.js";
 import connectPgSimple from "connect-pg-simple";
 import bcrypt from "bcrypt";
 
-//comments for pushing changes as
+//comments for pushing changes asdfd
 
 //import logRoutes from "./routes/logRoutes";
 //import settingsRoutes from "./routes/settingsRoutes";
