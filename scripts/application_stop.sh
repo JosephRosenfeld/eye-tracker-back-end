3776be09e2a04fc34bedeb5d982f9c165583e923
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 11:29:42 -0500

    edit to push

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing servers (echo without stdout specified)"  
-
+echo "Stopping any existing servers (echo without stout specified)"  
 {
 echo "Stopping any existing node servers!"  
 pkill -9 node 
-pkill -9 ngrok
+pkill -9 ngrok  
 } > app_stop_.out.log 2> app_stop.err.log
