455d7bd41eeaeb28225ddf6641d471eaea78b807
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 10:54:11 -0500

    changed appsepc and gave app_stop an out

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers !"
-pkill -9 node &>/dev/null
+pkill -9 node > app_stop.out.log 2> app_stop.err.log < /dev/null &
