bdcea48167d5df4920fc2cb8106ca9343ffe7f43
Joseph <josephgrosenfeld@gmail.com>
2022-01-28 21:25:10 -0500

    added trust proxy

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import bcrypt from "bcrypt";
 const pgSession = connectPgSimple(session);
 dotenv.config();
 const app = express();
+app.set("trust proxy", 1); // trust first proxy
 
 /*--- Middlewares ---*/
 //Need cors options since we're dealing with cookies
